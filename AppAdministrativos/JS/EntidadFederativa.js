a71bbe9008912bc15f15d7ab5f86a95b2fec5de3
j053pepe <j053_pepe@outlook.com>
2017-06-30 10:04:13 -0500

    Cambios de Script para optimizacion y html se quitaron las refrencias de hosjas de estilo y Js # Modulos *Nuevo Ingreso *Inscripcion Alumno

--- AppAdministrativos/JS/EntidadFederativa.js
+++ AppAdministrativos/JS/EntidadFederativa.js
@@ -1,11 +1,11 @@
 ﻿var Estado = function () {
     var cargarEstado = function () {
+        $('#Load').modal('show');
         $.ajax({
             type: "POST",
             url: "WS/General.asmx/ConsultarEntidadFederativa",
-            data: "{}", // the data in form-encoded format, ie as it would appear on a querystring
-            //contentType: "application/x-www-form-urlencoded; charset=UTF-8", // if you are using form encoding, this is default so you don't need to supply it
-            contentType: "application/json; charset=utf-8", // the data type we want back, so text.  The data will come wrapped in xml
+            data: "{}",
+            contentType: "application/json; charset=utf-8", 
             success: function (data) {
                 var datos = data.d;
                 $(datos).each(function () {
@@ -16,7 +16,6 @@
 
                     $("#slcEstado").append(option);
                 });
-                //$("#slcSexo").html(data); // show the string that was returned, this will be the data inside the xml wrapper
                 $("#slcEstado").val('9');
                 $('#slcEstado').change();
             }
@@ -48,6 +47,7 @@
 
                     $("#slcMunicipio").append(option);
                 });
+                $('#Load').modal('hide');
             }
         });
     });
