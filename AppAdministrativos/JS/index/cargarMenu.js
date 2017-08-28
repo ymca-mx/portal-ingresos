abe0112cbf93e2ecb70feefa1003ddc33349d1a6
j053pepe <j053_pepe@hotmail.com>
2017-08-28 13:17:26 -0500

    Actualizacion para menu, se agrego BackBone para manejo de historial y de Actualizacion.

--- AppAdministrativos/JS/index/cargarMenu.js
+++ AppAdministrativos/JS/index/cargarMenu.js
@@ -1,55 +1,79 @@
-﻿$(document).ready(function () {
-    Menu();
-    function Menu() {
-        var Menu = "";
+﻿$(function init() {
+    var Funciones = {
+        router: function () {
+            AppRouter = Backbone.Router.extend({
+                routes: {
+                    "*actions": "defaultRoute"
+                },
+            });
+            // Initiate the router
+            var app_router = new AppRouter;
 
-        $.blockUI({
-            message: "<h1>Cargando Menu, por favor espere....</h1>",
-            css: { backgroundColor: '#48525e', color: '#fff' }
-        });
+            app_router.on('route:defaultRoute', function (actions) {
+                $('#divDinamico').empty();
+                if (actions === null) { return false; }
+                var url = actions;
+                //console.log('Perras');
+                $('#divDinamico').load(url);
+            });
 
-        $.ajax({
-            url: 'WS/Usuario.asmx/ConsultarMenu',
-            type: 'POST',
-            contentType: 'application/json; charset=utf-8',
-            data: "{'usuarioId':'" + $.cookie('userAdmin') + "'}",
-            dataType: 'json',
-            success: function (Resultado) {
-                Datos = Resultado.d;
-                if (Datos == null) {
-                    alert('Error en la carga del menu');
-                    $.unblockUI();
-                }
-                else {
-                    $(Resultado.d).each(function () {
-                        Menu += '<li class="menu-dropdown mega-menu-dropdown ">' +
-                            '<a class="dropdown-toggle"  data-hover="megamenu-dropdown" data-close-others="true" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">'+
+            // Start Backbone history a necessary step for bookmarkable URL's
+            Backbone.history.start();
 
-                            this.Descripcion +
+        },
+        Menu: function () {
+            var Menu = "";
 
-                            ' <i class="fa fa-angle-down"></i>' +
-                            '</a>' +
-                            '<ul class="dropdown-menu pull-left">';
-                        $(this.SubMenu).each(function (ind, ele) {
-                            Menu +=
-                             '<li class="dropdown-submenu">' +
-                            '<a href="' + ele.Direccion + '" class="contenido">' +
-                             '<i class="fa fa-history"></i>'
-                             + ele.Descripcion +
-                             '</a>' +
-                             '</li>';
+            $.blockUI({
+                message: "<h1>Cargando Menu, por favor espere....</h1>",
+                css: { backgroundColor: '#48525e', color: '#fff' }
+            });
+
+            $.ajax({
+                url: 'WS/Usuario.asmx/ConsultarMenu',
+                type: 'POST',
+                contentType: 'application/json; charset=utf-8',
+                data: "{'usuarioId':'" + $.cookie('userAdmin') + "'}",
+                dataType: 'json',
+                success: function (Resultado) {
+                    Datos = Resultado.d;
+                    if (Datos == null) {
+                        alert('Error en la carga del menu');
+                        $.unblockUI();
+                    }
+                    else {
+                        $(Resultado.d).each(function () {
+                            Menu += '<li class="menu-dropdown mega-menu-dropdown ">' +
+                                '<a class="dropdown-toggle"  data-hover="megamenu-dropdown" data-close-others="true" data-toggle="dropdown" href="#index.html" aria-haspopup="true" aria-expanded="false">' +
+
+                                this.Descripcion +
+
+                                ' <i class="fa fa-angle-down"></i>' +
+                                '</a>' +
+                                '<ul class="dropdown-menu pull-left">';
+                            $(this.SubMenu).each(function (ind, ele) {
+                                Menu +=
+                                    '<li class="dropdown-submenu">' +
+                                    '<a href="#' + ele.Direccion + '" class="contenido">' +
+                                    '<i class="fa fa-history"></i>'
+                                    + ele.Descripcion +
+                                    '</a>' +
+                                    '</li>';
+                            });
+                            Menu += '</ul>' + '</li>';
                         });
-                        Menu += '</ul>' + '</li>';
-                    });
-                    $('#Menu').append(Menu);
+                        $('#Menu').append(Menu);
+                        $.unblockUI();
+                        Funciones.router();
+                    }
+                },
+                error: function (Resultado) {
+                    alert('Se presento un error en la validación de las credenciales');
                     $.unblockUI();
+                    $(location).attr('href', 'Login.html');
                 }
-            },
-            error: function (Resultado) {
-                alert('Se presento un error en la validación de las credenciales');
-                $.unblockUI();
-                $(location).attr('href', 'Login.html');
-            }
-        });
-    }
+            });
+        }
+    };
+    Funciones.Menu();
 });
--- AppAdministrativos/JS/index/index.js
+++ AppAdministrativos/JS/index/index.js
@@ -1,35 +1,44 @@
-﻿$(document).ready(function () {
+﻿$(function init() {
 
-    $("#Menu").on('click', '.contenido', function () {
+    //$("#Menu").on('click', '.contenido', function () {
 
-        $('#divDinamico').empty();
-        var url = $(this).attr("href");
-        $('#divDinamico').load(url);
-        return false;
-    });
+    //    $('#divDinamico').empty();
+    //    var url = $(this).attr("href");
+    //    $('#divDinamico').load(url);
+    //    return false;
+    //});
+    var Funciones ={        
+        btnSalir: function () {
+            $.removeCookie('userAdmin', { path: '/' });
+            var url = $(this).attr("href");
+            $(location).attr('href', url);
+        },
+        TraerImagen: function () {
+            $.ajax({
+                url: 'WS/Usuario.asmx/Datos',
+                type: 'POST',
+                contentType: 'application/json; charset=utf-8',
+                data: "{'usuarioId':'" + $.cookie('userAdmin') + "'}",
+                dataType: 'json',
+                success: function (Resultado) {
+                    Datos = Resultado.d;
+                    if (Datos == null)
+                        alert('Error en la carga de datos generales');
+                    else {
 
-     $.ajax({
-         url: 'WS/Usuario.asmx/Datos',
-         type: 'POST',
-         contentType: 'application/json; charset=utf-8',
-         data: "{'usuarioId':'" + $.cookie('userAdmin') + "'}",
-         dataType: 'json',
-         success: function (Resultado) {
-             Datos = Resultado.d;
-             if (Datos == null)
-                 alert('Error en la carga de datos generales');
-             else {
+                        $('#imgUsuario').attr('src', 'data:image/' + Datos.extensionImagen + ';base64,' + Datos.imagenBase64);
+                        $('#lblUsuario span').text(Datos.nombre);
 
-                 $('#imgUsuario').attr('src', 'data:image/' + Datos.extensionImagen + ';base64,' + Datos.imagenBase64);
-                 $('#lblUsuario span').text(Datos.nombre);
-
-                 if (Datos.extensionImagen == ".png")
-                     $('#imgUsuario').attr('src', 'Style/engato/index/imagenes/Guest.png');
-             }
-         },
-         error: function (Resultado) {
-             alert('Se presento un error en la validación de las credenciales');
-         }
-     });
-   
+                        if (Datos.extensionImagen == ".png")
+                            $('#imgUsuario').attr('src', 'Style/engato/index/imagenes/Guest.png');
+                    }
+                },
+                error: function (Resultado) {
+                    alert('Se presento un error en la validación de las credenciales');
+                }
+            });
+        }
+    }
+    Funciones.TraerImagen();
+    $('#btnSalir').on('click', btnSalir);
 });
