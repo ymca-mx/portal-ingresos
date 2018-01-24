e3e8f6662f7a9917d062b33ce071a234d3d120dd
j053pepe <j053_pepe@hotmail.com>
2018-01-24 09:38:12 -0600

    Cambios para Alumnos.asmx se agregaron algunas referencias. AlumnoDatos.js se agrego codigo para actualizar en CFDI y se modifico la forma de envio de parametros al momento de guardar. En el proyecto de pruebas se agrego nueva prueba para conectarse a la api de CFDI se elimino proyecto de antonio.

---
+++ Pruebas/CFDI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class CFDI
+    {
+        HttpClient client = new HttpClient();
+
+        [TestMethod]
+        public async Task PruebasApi()
+        {
+            client.BaseAddress = new Uri("http://localhost:44314/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            object ComplementoEducativo = new
+            {
+                alumnoId = 803,
+                nombre = "Demetrio Valdez Alfaro",
+                curp = "acvbsdfg123456789"
+            };
+
+
+            string obj = await UpdateCFDI(ComplementoEducativo);
+            JObject o = JObject.Parse(obj);
+
+
+            Console.WriteLine("Termine");
+            Console.WriteLine("Resultado..." + (string)o["message"]);
+        }
+
+        async Task<string> UpdateCFDI(object ComplementoEducativo)
+        {
+            HttpResponseMessage obj = await client.PostAsJsonAsync(
+                "api/Universal/EditComplementoEducativo", ComplementoEducativo);
+
+            return obj.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
